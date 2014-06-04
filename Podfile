aed114e889a5f68383a3e65ff50d9de64e82db62
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-06-04 13:57:00 +0200

    added some labels to the view controller. Added ESTBeacon library.

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, '7.0'
 inhibit_all_warnings!
 
+pod 'EstimoteSDK'
 
 target :RoomsTests, :exclusive => true do
     pod 'Kiwi/XCTest', '~> 2.2'
--- Rooms/TUIViewController.m
+++ Rooms/TUIViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "TUIViewController.h"
+// Beacons
+#import "ESTBeaconManager.h"
 
-@interface TUIViewController ()
+
+@interface TUIViewController () <ESTBeaconManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *proximityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rssiLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfBeaconsLabel;
+
+@property (nonatomic, strong) ESTBeaconManager* beaconManager;
 
 @end
 
@@ -18,12 +30,40 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // create manager instance
+    self.beaconManager = [[ESTBeaconManager alloc] init];
+    self.beaconManager.delegate = self;
+    
+    // create sample region object (you can additionaly pass major / minor values)
+    ESTBeaconRegion* region = [[ESTBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:@"54fbdbd7-621f-4153-9466-47c86ecf886b"] identifier:@"testRegion"];
+    
+    // start looking for estimote beacons in region
+    // when beacon ranged beaconManager:didRangeBeacons:inRegion: invoked
+    [self.beaconManager startRangingBeaconsInRegion:region];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)beaconManager:(ESTBeaconManager *)manager
+     didRangeBeacons:(NSArray *)beacons
+            inRegion:(ESTBeaconRegion *)region
+{
+    
+    if([beacons count] == 1)
+    {
+        for (ESTBeacon* beacon in beacons)
+        {
+            _majorLabel.text = [[beacon major] stringValue];
+            _minorLabel.text = [[beacon minor] stringValue];
+            _distanceLabel.text = [[beacon distance] stringValue];
+            //TO DO: go on with the rest of labels
+            
+        }
+    }
 }
 
 @end
